7668c8504da0a12c5e9fe3614d0b1ee1a7dd9da3
Cristina Huertas <120569275+CrisStudyZone@users.noreply.github.com>
2026-03-19 18:53:56 -0300

    Add shared Compose string resources for UI text

--- composeApp/src/commonMain/kotlin/com/serdigital/myapplication/App.kt
+++ composeApp/src/commonMain/kotlin/com/serdigital/myapplication/App.kt
@@ -6,18 +6,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.spacedBy
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 import myapplication.composeapp.generated.resources.Res
 import myapplication.composeapp.generated.resources.compose_multiplatform
+import myapplication.composeapp.generated.resources.play
+import myapplication.composeapp.generated.resources.start_practice
+import myapplication.composeapp.generated.resources.welcome
 
 @Composable
 @Preview
@@ -29,19 +36,33 @@ fun App() {
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
+            verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+            Text(
+                text = stringResource(Res.string.welcome),
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            Column(
+                modifier = Modifier.padding(top = 16.dp),
+                verticalArrangement = spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Button(onClick = { showContent = !showContent }) {
+                    Text(stringResource(Res.string.play))
+                }
+                Button(onClick = { showContent = !showContent }) {
+                    Text(stringResource(Res.string.start_practice))
+                }
             }
             AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
                 Column(
-                    modifier = Modifier.fillMaxWidth(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
                 }
             }
         }
